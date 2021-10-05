2078eb09b2d7e411b52544ec86de8e3667d32446
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2021-10-05 10:17:30 +0300

    fixed media

--- Components/Definition/def.js
+++ Components/Definition/def.js
@@ -1,28 +1,30 @@
 import styles from "./def.module.css"
 import About from "../About/about"
 
- 
-export default function def({ Def, das, naf, hep}) {
+
+export default function def({ Def, das, naf, hep }) {
 
     return (
-        <div className={styles.container} style={{ background: `url(${Def[0].poster.asset.url})`, backgroundSize: "cover" }}>
+        <div className={styles.allcontainer}>
 
-            <div className={styles.back} key={Def[0]} >
+            <div className={styles.container} style={{ background: `url(${Def[0].poster.asset.url})`, backgroundSize: "cover" }}>
+                <div className={styles.back} key={Def[0]} >
 
-                <h1 className={styles.h1} key={Def[0].title}>{Def[0].title}</h1>
+                    <h1 className={styles.h1} key={Def[0].title}>{Def[0].title}</h1>
 
-                <div className={styles.textcontainer} key={Def[0].description}><p className={styles.p1}>{Def[0].description}</p></div>
+                    <div className={styles.textcontainer} key={Def[0].description}><p className={styles.p1}>{Def[0].description}</p></div>
 
-                <div className={styles.About}>
+                    <div className={styles.About}>
 
-                    <About das={das} naf={naf} hep={hep}  />
+                        <About das={das} naf={naf} hep={hep} />
+
+                    </div>
 
                 </div>
-
             </div>
 
-
         </div>
+
     )
 }
 
